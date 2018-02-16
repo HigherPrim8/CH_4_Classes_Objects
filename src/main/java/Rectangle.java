0219a0b5c96e418aa1fe91d45a41773fe2bd3175
Ross Hall <rhall@commandalkon.com>
2018-02-16 07:00:37 -0700

    Ch 4 from Ross

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,4 +1,5 @@
 public class Rectangle {
+
 	/*
 	 * Design a class named Rectangle to represent a rectangle. It should contains
 	 *  - Two double data fields named width and height. The default values for both is 1.
@@ -8,4 +9,35 @@ public class Rectangle {
 	 *  - A method named getArea() that returns the are of this rectangle A = l*w
 	 *  - A method named getPerimeter() that returns the perimeter of this rectangle P = 2 * (l+w)
 	 */
+	private double width, height;
+
+	// A no-arg constructor that creates a default rectangle
+	public Rectangle() {
+		width = 1;
+		height = 1;
+	}
+
+	// A constructor that creates a rectangle with the specified width and height
+	public Rectangle(double h, double wid) {
+		width = wid;
+		height = h;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	// A method named getArea() that returns the are of this rectangle A = l*w
+	public double getArea() {
+		return width * height;
+	}
+
+	// A method named getPerimeter() that returns the perimeter of this rectangle P = 2 * (l+w)
+	public double getPerimeter() {
+		return 2 * (width + height);
+	}
 }
--- src/main/java/Stock.java
+++ src/main/java/Stock.java
@@ -2,14 +2,65 @@ public class Stock {
 
 	/**
 	 * Design a class named Stock. The Stock class should contain the following
-	 *  - A string data field named symbol for the stock's symbol
-	 *  - A string data field named name for the stock's name
-	 *  - A double data field named previousClosingPrice that stores the stocks price for the previous day
-	 *  - A double data field named currentPrice that stores the stock price for the current time
-	 *  - A constructor that creates a stock with specified symbol and name
-	 *  - Getters for each of the data fields
-	 *  - Setter method for previousClosingPrice and currentPrice
-	 *  - A method named getChangePercent() that returns the percentage changed from previousClosingPrice to CurrentPrice
-	 *    precentChange = ((currentPrice - closingPrice) / currentPrice) * 100
+	 * - A string data field named symbol for the stock's symbol
+	 * - A string data field named name for the stock's name
+	 * - A double data field named previousClosingPrice that stores the stocks price for the previous day
+	 * - A double data field named currentPrice that stores the stock price for the current time
+	 * - A constructor that creates a stock with specified symbol and name
+	 * - Getters for each of the data fields
+	 * - Setter method for previousClosingPrice and currentPrice
+	 * - A method named getChangePercent() that returns the percentage changed from previousClosingPrice to CurrentPrice
+	 * precentChange = ((currentPrice - closingPrice) / currentPrice) * 100
 	 */
+
+	private String Symbol;
+	private String Name;
+	private double previousClosingPrice;
+	private double currentPrice;
+
+	// A constructor that creates a stock with specified symbol and name
+	public Stock(String s, String n) {
+		this.Symbol = s;
+		this.Name = n;
+	}
+
+	// Getters for each of the data fields
+	public String getSymbol() {
+		return this.Symbol;
+	}
+
+	public String getName() {
+		return this.Name;
+	}
+
+	public double getCurrentPrice() {
+		return currentPrice;
+	}
+
+	public double getPreviousClosingPrice() {
+		return previousClosingPrice;
+	}
+
+	// Setter method for previousClosingPrice and currentPrice
+	public void setSymbol(String s) {
+		this.Symbol = s;
+	}
+
+	public void setName(String n) {
+		this.Name = n;
+	}
+
+	public void setPreviousClosingPrice(double pcp) {
+		this.previousClosingPrice = pcp;
+	}
+
+	public void setCurrentPrice(double cp) {
+		this.currentPrice = cp;
+	}
+
+	// A method named getChangePercent() that returns the percentage changed from previousClosingPrice to CurrentPrice
+	// precentChange = ((currentPrice - closingPrice) / currentPrice) * 100
+	public double getChangePercent() {
+		return ((currentPrice - previousClosingPrice) / currentPrice) * 100;
+	}
 }
